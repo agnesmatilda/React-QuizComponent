12035269d4710f3cf46c63707dfdd64eb90ab01c
ML <agnesmatilda24@hotmail.com>
2020-09-16 00:10:13 +0200

    my

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,11 +10,10 @@ class Quiz extends Component {
         this.state = { quiz_position: 1 }
     }
     render() {
-        const isQuizEnd = (this.state.quiz_position - 1 === quizData.quiz_questions.length)
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         return (
             <div>
-                //{isQuizEnd === 'true' && <div QuizEnd />}
-                {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+                {isQuizEnd === 'true' && <div QuizEnd />}
                 <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]} />
             </div>
         )
