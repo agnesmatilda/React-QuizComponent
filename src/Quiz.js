9887751400edc53c3450ff2db69f58af253006f7
ML <agnesmatilda24@hotmail.com>
2020-09-16 00:07:43 +0200

    right way

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,7 +13,8 @@ class Quiz extends Component {
         const isQuizEnd = (this.state.quiz_position - 1 === quizData.quiz_questions.length)
         return (
             <div>
-                {isQuizEnd === 'true' && <div QuizEnd />}
+                //{isQuizEnd === 'true' && <div QuizEnd />}
+                {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
                 <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]} />
             </div>
         )
