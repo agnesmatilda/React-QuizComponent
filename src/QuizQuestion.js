7d2bfa342897ed8c9e04ec6918f334064ea7293e
ML <agnesmatilda24@hotmail.com>
2020-09-15 20:09:28 +0200

    ssssssssss

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,7 +5,7 @@ class QuizQuestion extends Component {
         return (
             <main>
         <section>
-                    <p>Quiz.props.instruction_text</p>
+                    <p>this.props.quiz_question.instruction_text</p>
         </section>
         <section className="buttons">
                 <ul>
