149761b3f31928d0d67513ae96f0e0ff9e9ae20d
ML <agnesmatilda24@hotmail.com>
2020-09-16 14:07:38 +0200

    Once more

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -25,7 +25,7 @@ class QuizQuestion extends Component {
             this.props.showNextQuestionHandler()
         }
         this.setState(
-            incorrectAnswer = true > false ? 'false' : 'true'
+            state = true > false ? 'false' : 'true'
         )
     }
 }
