06c6ef8d9423a78fc9e4997b7f875a79e7e0ce08
ML <agnesmatilda24@hotmail.com>
2020-09-16 12:10:38 +0200

    6,8

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,8 +10,9 @@ class Quiz extends Component {
         this.state = { quiz_position: 1 }
     }
     showNextQuestion() {
-        this.setState(this.state.quiz_position = 1)
-    }
+        this.setState((state) => {
+            return { quiz_position:state.quiz_position}
+    })
     render() {
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         return (
