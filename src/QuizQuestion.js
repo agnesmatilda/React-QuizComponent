559340003457a0966057beac70bc9e788d04abec
ML <agnesmatilda24@hotmail.com>
2020-09-16 11:54:47 +0200

    6,7 again

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -17,7 +17,7 @@ class QuizQuestion extends Component {
       </main>)
     }
     handleClick(buttonText) {
-        if (this.props.quiz_question.answer = buttonText) {
+        if (this.props.quiz_question.answer === buttonText) {
             this.props.showNextQuestionHandler()
         }
     }
