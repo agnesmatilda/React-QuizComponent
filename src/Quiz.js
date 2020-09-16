a6b750c49038e36d026f189a9188feff62eb2bd8
ML <agnesmatilda24@hotmail.com>
2020-09-16 12:00:35 +0200

    yes there is, try to move it

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,6 +9,9 @@ class Quiz extends Component {
         super(props)
         this.state = { quiz_position: 1 }
     }
+    showNextQuestion() {
+        this.setState(this.state.quiz_position = 1)
+    }
     render() {
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         return (
@@ -16,9 +19,6 @@ class Quiz extends Component {
                 {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/>}
             </div>
         )
-    }
-    showNextQuestion(){
-        this.setState(this.state.quiz_position = 1)
     }
 }
 
