fac74dcd1588f93b409d649dc91b941a792c971a
ML <agnesmatilda24@hotmail.com>
2020-09-16 17:43:12 +0200

    Moved some

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
 class QuizEnd extends Component {
+    handleResetClick() {
+        this.props.resetClickHandler()
+    }
     render() {
-        handleResetClick() {
-            this.props.resetClickHandler()
-        }
         return (
             <div>
                 <p>Thanks for playing!</p>
